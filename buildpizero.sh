f0d2a31f68316e780cdbfd9d6a3e82dbc56d015f
chromatic-wolf <caleb.davo@hotmail.com>
2020-10-01 15:28:47 +1000

    Updated build script and added build directory for binaries

--- buildpizero.sh
+++ buildpizero.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -o game main.cpp player.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17 -march=armv6zk -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard
+g++ -o binaries/rpiZero main.cpp player.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17 -march=armv6zk -mcpu=arm1176jzf-s -mtune=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard
