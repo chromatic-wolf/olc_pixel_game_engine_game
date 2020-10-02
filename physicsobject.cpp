a57fbb8b6f35ef5936a3f74525be360101281077
chromatic-wolf <caleb.davo@hotmail.com>
2020-10-02 18:59:17 +1000

    Added base class for physics objects

---
+++ physicsobject.cpp
@@ -0,0 +1,6 @@
+#include "physicsobject.h"
+
+PhysicsObject::PhysicsObject()
+{
+
+}
---
+++ physicsobject.h
@@ -0,0 +1,11 @@
+#ifndef PHYSICSOBJECT_H
+#define PHYSICSOBJECT_H
+
+
+class PhysicsObject
+{
+public:
+    PhysicsObject();
+};
+
+#endif // PHYSICSOBJECT_H
